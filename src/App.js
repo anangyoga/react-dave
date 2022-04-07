59e4ac551648c447bd3904e38094a7b4d71a760c
anangyoga <aharwinto@gmail.com>
2022-04-07 23:11:45 +0700

    jsx

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const handleRandomName = () => {
+    const names = ["Timo", "Walter", "Agnes", "Sochi"];
+    const int = Math.floor(Math.random() * 4);
+    return names[int];
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello
+          {handleRandomName()}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
