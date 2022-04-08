600aac2f318320ba58bf9e0d44325c02e9402ad8
anangyoga <aharwinto@gmail.com>
2022-04-08 09:58:36 +0700

    click events

--- src/Content.js
+++ src/Content.js
@@ -6,7 +6,24 @@ function Content() {
     const int = Math.floor(Math.random() * 4);
     return names[int];
   };
-  return <main>Hi {handleRandomName()}</main>;
+
+  const handleClick = () => {
+    console.log("clicked");
+  };
+  const handleClickName = (name) => {
+    console.log(`${name} was clicked`);
+  };
+  const handleClickEvent = (e) => {
+    console.log(e.target.innerText);
+  };
+  return (
+    <main>
+      <p onDoubleClick={handleClick}>Hi {handleRandomName()} </p>
+      <button onClick={handleClick}>Click It</button>
+      <button onClick={() => handleClickName("Tim")}>Show Name</button>
+      <button onClick={(e) => handleClickEvent(e)}>Event</button>
+    </main>
+  );
 }
 
 export default Content;
