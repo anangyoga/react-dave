d9504bbb35bd5aedb8b68567c390c4ef3da58529
anangyoga <aharwinto@gmail.com>
2022-04-09 14:05:27 +0700

    fetch data from api

--- src/Content.js
+++ src/Content.js
@@ -3,7 +3,7 @@ import React from "react";
 import ItemList from "./ItemList";
 
 function Content({ items, handleCheck, handleDelete }) {
-  return <main>{items.length ? <ItemList items={items} handleCheck={handleCheck} handleDelete={handleDelete} /> : <p style={{ marginTop: "2rem" }}>Your List Is Empty</p>}</main>;
+  return <>{items.length ? <ItemList items={items} handleCheck={handleCheck} handleDelete={handleDelete} /> : <p style={{ marginTop: "2rem" }}>Your List Is Empty</p>}</>;
 }
 
 export default Content;
